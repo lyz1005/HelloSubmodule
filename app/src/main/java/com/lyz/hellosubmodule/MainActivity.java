f19552158538abdf3634b5aacc288affd47dfb0d
luyuzhi <lyz100417@163.com>
2017-06-13 15:41:15 +0800

    invoke first module

--- app/src/main/java/com/lyz/hellosubmodule/MainActivity.java
+++ app/src/main/java/com/lyz/hellosubmodule/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lyz.hellosubmodule;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import com.lyz.test.Test;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText(Test.getDesc());
     }
 }
